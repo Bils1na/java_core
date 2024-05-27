6fca00bbaa8aadffcc455428624ea920566143e3
bilsina <bils1n@icloud.com>
2024-05-27 16:19:29 +0500

    added hw5

--- src/main/java/org/example/hw5/Program.java
+++ src/main/java/org/example/hw5/Program.java
@@ -4,12 +4,15 @@ import java.io.*;
 
 public class Program {
 
+    private static final String PARENT_DIRECTORY = ".";
+
+
     public static void main(String[] args) throws IOException {
-        createBackup(new File("."));
+        createBackup(new File(PARENT_DIRECTORY));
     }
 
     private static void createBackup(File parentDirectory) throws IOException {
-        makeDirectory(".", "backup");
+        makeDirectory(PARENT_DIRECTORY, "backup");
 
         File[] files = parentDirectory.listFiles();
         for (File file : files) {
@@ -19,15 +22,24 @@ public class Program {
 
     private static void copyFile(File fileName) throws IOException {
         File source = new File(String.valueOf(fileName));
-        File copy = new File(String.format("%s", fileName));
-        try (FileInputStream fileInputStream = new FileInputStream(source)) {
-            int c = 0;
-            try (FileOutputStream fileOutputStream = new FileOutputStream(copy)) {
-                while ((c = fileInputStream.read()) != -1) {
-                    fileOutputStream.write(c);
+        File copy = new File(String.format(".\\backup\\%s", fileName));
+
+        if (source.isDirectory()) {
+             makeDirectory(".\\backup\\", String.format(source.getName()));
+             for (File subFile : fileName) {
+
+             }
+        } else {
+            try (FileInputStream fileInputStream = new FileInputStream(source)) {
+                int c = 0;
+                try (FileOutputStream fileOutputStream = new FileOutputStream(copy)) {
+                    while ((c = fileInputStream.read()) != -1) {
+                        fileOutputStream.write(c);
+                    }
                 }
             }
         }
+
     }
 
     private static void makeDirectory(String currentDirectory, String newDirectoryName) {
