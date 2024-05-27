41563032ec59d809c893b2908e5ca6443bfb8c05
bilsina <bils1n@icloud.com>
2024-05-27 15:46:13 +0500

    added hw5

--- src/main/java/org/example/hw5/Program.java
+++ src/main/java/org/example/hw5/Program.java
@@ -1,11 +1,32 @@
 package org.example.hw5;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        makeDirectory(".");
+        try (FileOutputStream fileOutputStream = new FileOutputStream("./backup/file.txt")) {
+            fileOutputStream.write("Hello".getBytes());
+        }
+    }
+
+    private static void createBackup() {
 
     }
 
+    private static void copyFile(File fileName) {
 
+    }
+
+    private static void makeDirectory(String currentDirectory) {
+        File newDirectory = new File(currentDirectory, "backup");
+
+        if (!newDirectory.exists()) {
+            newDirectory.mkdir();
+        }
+    }
 
 }
