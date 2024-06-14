dfb987484c3ad7465df2202cf860633e1d45c3d6
bilsina <bils1n@icloud.com>
2024-06-14 15:18:28 +0500

    added sem1_jdk

---
+++ src/main/java/org/example/jdk_hw1/Program.java
@@ -0,0 +1,4 @@
+package org.example.jdk_hw1;
+
+public class Program {
+}
---
+++ src/main/java/org/example/jdk_sem1/Program.java
@@ -0,0 +1,9 @@
+package org.example.jdk_sem1;
+
+public class Program {
+
+    public static void main(String[] args) {
+        new GameWindow();
+    }
+
+}
