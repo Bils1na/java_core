86a0f172d84ca31a4aa420c3fb24cfdffd27345c
bilsina <bils1n@icloud.com>
2024-07-01 14:47:05 +0500

    Добавлено sem6.

---
+++ maven-test/maven_test/src/main/java/org/artem/App.java
@@ -0,0 +1,13 @@
+package org.artem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
